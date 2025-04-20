311a22da2363bc182d08668ae35bf555c144b1f9
Jasch Dhungel <53111523+Jasch0Jumper@users.noreply.github.com>
2025-04-20 15:54:51 +0200

    added cars that drive in circles

--- gepard/simulation.js
+++ gepard/simulation.js
@@ -1,16 +1,16 @@
 
 const direction = {
-    up: 0,
-    right: 1,
-    down: 2,
-    left: 3,
+    up: {x: 0, y: -1},
+    right: {x: 1, y: 0},
+    down: {x: 0, y: 1},
+    left: {x: -1, y: 0},
 };
 
 const cellType = {
     empty: 0,
     road: 1,
     car: 2,
-}
+};
 
 const car = {
     x: 0,
@@ -28,6 +28,7 @@ const grid = {
 const Simulation = {  
     state: {
         running: false,
+        roads: [],
         cars: [],
     },
 
@@ -50,52 +51,80 @@ const Simulation = {
 
         this.createCircularRoad(5);
         this.createCircularRoad(6);
+
+        for (let i = 0; i < this.numberOfCars; i++) {
+            const randomRoad = random(this.state.roads)
+            this.createCar(randomRoad);
+        }
     },
 
     createCircularRoad: function(distance) {
         // Create horizontal roads
         for (let x = distance; x < grid.width - distance; x++) {
-            grid.cells[x][distance] = cellType.road;
-            grid.cells[x][grid.height - distance - 1] = cellType.road;
+            this.addRoad(x, distance, direction.right);
+            this.addRoad(x, grid.height - distance - 1, direction.left);
         }
         // Create vertical roads
         for (let y = distance; y < grid.height - distance; y++) {    
-            grid.cells[distance][y] = cellType.road;
-            grid.cells[grid.width - distance - 1][y] = cellType.road;
+            this.addRoad(distance, y, direction.up);
+            this.addRoad(grid.width - distance - 1, y, direction.down);
         }
+
+        this.state.roads.find(road => road.x === distance && road.y == distance).direction = direction.right;
+        this.state.roads.find(road => road.x === grid.width - distance - 1 && road.y == distance).direction = direction.down;
+        this.state.roads.find(road => road.x === distance && road.y == grid.height - distance - 1).direction = direction.up;
+        this.state.roads.find(road => road.x === grid.width - distance - 1 && road.y == grid.height - distance - 1).direction = direction.left;
+    },
+
+    addRoad: function(x, y, direction){
+        const newRoad = {
+            x: x,
+            y: y,
+            direction: direction,
+        }
+        this.state.roads.push(newRoad);
+        grid.cells[x][y] = cellType.road;
     },
 
     drawGrid: function() {
-        for (let i = 0; i < grid.width; i++) {
-            for (let j = 0; j < grid.height; j++) {
+        for (let x = 0; x < grid.width; x++) {
+            for (let y = 0; y < grid.height; y++) {
                 const offsetX = (width - grid.width * this.cellSize) / 2;
                 const offsetY = (height - grid.height * this.cellSize) / 2;
 
-                if (grid.cells[i][j] === cellType.empty) {
+                if (grid.cells[x][y] === cellType.empty) {
                     fill(0, 0, 0);
-                } else if (grid.cells[i][j] === cellType.road) {
+                } else if (grid.cells[x][y] === cellType.road) {
                     fill(100, 100, 100);
-                } else if (grid.cells[i][j] === cellType.car) {
-                    fill(0, 200, 0);
+                } else if (grid.cells[x][y] === cellType.car) {
+                    const car = this.state.cars.find(car => car.x === x && car.y === y);
+                    if (car.direction === direction.up) {
+                        fill(0, 200, 0);
+                    } else if (car.direction === direction.down) {
+                        fill(0, 0, 200);
+                    } else if (car.direction === direction.left) {
+                        fill(200, 0, 200);
+                    } else if (car.direction === direction.right) {
+                        fill(0, 200, 200);
+                    }
                 } else {
                     fill(255, 255, 255);
                 }
                 
-                rect(i * this.cellSize + offsetX, j * this.cellSize + offsetY, this.cellSize, this.cellSize);
+                rect(x * this.cellSize + offsetX, y * this.cellSize + offsetY, this.cellSize, this.cellSize);
             }
         }
     },
 
-    createCars: function() {
-        for (let i = 0; i < this.numberOfCars; i++) {
-            const newCar = {
-                x: random(5, grid.width - 5),
-                y: random(5, grid.height - 5),
-                speed: 1,
-                direction: random(0, 3),
-            };
-            this.state.cars.push(newCar);
-        }
+    createCar: function(road) {
+        const newCar = {
+            x: road.x,
+            y: road.y,
+            speed: 1,
+            direction: road.direction,
+        };
+        this.state.cars.push(newCar);
+        grid.cells[newCar.x][newCar.y] = cellType.car;
     },
 
     start: function() {
@@ -111,7 +140,18 @@ const Simulation = {
     update: function() {
         if (!this.state.running) return;
         
-        
+        for (let i = 0; i < this.state.cars.length; i++) {
+            const car = this.state.cars[i];
+            this.moveCar(car);
+        }
         this.drawGrid();
+    },
+
+    moveCar: function(car) {
+        const road = this.state.roads.find(road => road.x === car.x && road.y === car.y);
+        grid.cells[car.x][car.y] = cellType.road;
+        car.x += road.direction.x;
+        car.y += road.direction.y;
+        grid.cells[car.x][car.y] = cellType.car;
     },
 }; 
